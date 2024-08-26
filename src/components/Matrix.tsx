5a9bf2b3715e514a08261c68163a6e8762a51e2e
Zeya Imran <khanzeya63@gmail.com>
2024-08-26 18:48:27 +0530

    fix the ui and some logic for rending the matrix

--- src/components/Matrix.tsx
+++ src/components/Matrix.tsx
@@ -12,26 +12,28 @@ const Matrix = ({
     newMatrix[row][col] = value;
     onMatrixChange(newMatrix);
   };
-
+  const numColumns = matrix.length > 0 ? matrix[0].length : 1;
+  console.log(numColumns);
   return (
     <div className="container mx-auto p-6 border">
-      <div className="grid grid-cols-3 gap-2">
+      <div
+        className="grid"
+        style={{
+          gridTemplateColumns: `repeat(${numColumns}, minmax(0, 1fr))`, // Dynamic grid columns
+        }}
+      >
         {matrix.map((row, i) =>
           row.map((cell, j) => (
             <input
               key={`${i}-${j}`}
-              placeholder={`${i + 1},${j + 1}`}
+              placeholder={`${i},${j}`}
               type="number"
               value={cell === 0 ? "" : cell}
               onChange={(e) => handleChange(Number(e.target.value), i, j)}
               className="
-              border 
-              p-2 
-              rounded 
-              w- 6/12
+              p-1
+              border border-gray-500
               text-center 
-              focus:outline-none 
-              focus:ring-2 focus:ring-green-500
                text-gray-900"
             />
           ))
--- src/components/MatrixGenerator.tsx
+++ src/components/MatrixGenerator.tsx
@@ -5,16 +5,22 @@ const MatrixGenerator = ({
 }: {
   onGenerate: (rows: number, columns: number) => void;
 }) => {
-  const [rows, setRows] = useState<number>(0);
-  const [columns, setColumns] = useState<number>(0);
+  const [rows, setRows] = useState<number | undefined>();
+  const [columns, setColumns] = useState<number | undefined>();
+
+  const handleGenerate = () => {
+    const rowCount = rows || 0;
+    const colCount = columns || 0;
+
+    onGenerate(rowCount, colCount);
+  };
 
   return (
     <div className="container md:mx-auto md:p-6 bg-white shadow-md rounded-md  py-8">
       <h2 className="text-2xl font-bold text-center mb-4 text-gray-700">
         Matrix Generator
       </h2>
-      <h1>{/* {rows} {columns} */}</h1>
-      <div className="grid md:grid-cols-3 md:gap-10 md:px-60 px-4 gap-4">
+      <div className="grid md:grid-cols-3 md:gap-10 md:px-60 px-4 gap-4 text-red-600">
         <input
           type="number"
           placeholder="Rows"
@@ -30,7 +36,7 @@ const MatrixGenerator = ({
           className="border p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500  text-gray-900"
         />
         <button
-          onClick={() => onGenerate(rows, columns)}
+          onClick={handleGenerate}
           className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           Generate Matrices
